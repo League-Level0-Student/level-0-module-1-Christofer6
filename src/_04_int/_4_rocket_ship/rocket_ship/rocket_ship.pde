f34224ca58985d773e0bd0df69c7c2575cbd9339
student <student@ip-172-31-62-175.us-west-2.compute.internal>
2021-01-21 12:30:36 -0800

    Bager

--- src/_04_int/_4_rocket_ship/rocket_ship/rocket_ship.pde
+++ src/_04_int/_4_rocket_ship/rocket_ship/rocket_ship.pde
@@ -3,18 +3,20 @@ int y = 600;
 
 void setup() {
     size(800, 800);
-    background(0, 0, 40); 
+    
 }
 
 void draw() {
-
+    background(0, 0, 40); 
     fill(random(255), 0, 0);
     ellipse(x, y + 130, 90, 90);
     fill(248, 128, 0);
     ellipse(x, y + 115, 70, 70);
     fill(255, 153, 0);
-    ellipse(x, y + 95, 35, 35);
+    ellipse(x, y + 97, 35, 35);
     fill(100, 100, 100);
     triangle(x, y + 10, x + 50, y + 100, x - 50, y + 100);
 
+    y -= 8;
+    
 }
---
+++ src/_05_for_loops/_2_badgers/Badgers.java
@@ -0,0 +1,32 @@
+package _05_for_loops._2_badgers;
+
+public class Badgers {
+	public static void main(String[] args) {
+		for (int i = 0; i < 12; i++) {
+			System.out.println("badger");
+		}
+		
+		for (int i = 0; i < 2; i++) {
+			System.out.println("Mushroom");
+		
+		
+	}
+	
+		for (int i = 0; i < 12; i++) {
+			System.out.println("Bager");
+		
+	
+	}
+
+	for (int i = 0; i < 2; i++) {
+		System.out.println("Mushroom");
+	
+
+	}	
+
+	for (int i = 0; i < 1; i++) {
+		System.out.println("A Snake!!!");
+	}
+	}
+	}	
+	
